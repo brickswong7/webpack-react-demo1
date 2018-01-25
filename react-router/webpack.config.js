02d7758d37175b8c65e143583c93669e0f5c84ed
bricks27 <brickswong@163.com>
2018-01-25 22:53:18 +0800

    配置文件

---
+++ react-router/webpack.config.js
@@ -0,0 +1,14 @@
+module.export ={
+	entry:"./index.js",
+	output:{
+		filename:"bundle.js",
+		publicPath:""
+	},
+	module:{
+		loaders:[
+			{test:"/\.js/",exclude:/node_modules/,loader:'babel-loader?presets[]=es2015&presets[]=react' }
+		]
+	}
+}
+
+
